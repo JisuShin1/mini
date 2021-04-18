3f680d36b82016c73d56f77acf6ba1c620ca0755
Jisu <22000392@handong.edu>
2021-04-18 22:42:25 +0900

    product.c changed

--- product.c
+++ product.c
@@ -2,7 +2,7 @@
 
 int createProduct(Product *p){
 
-	getchar();
+        getchar();
     printf("제품명은? ");
     scanf("%s" , p->name);
     printf("중량은? ");
@@ -19,7 +19,7 @@ void readProduct(Product p){
     printf("%8s %4d %4d" , p.name, p.weight, p.price);
     for(int i=0; i<p.num; i++)
     printf("★");
- 
+
 
     printf("\n");
 }
@@ -69,51 +69,70 @@ int deleteProduct(Product *p){
     return 1;
 }
 
-int selectMenu(){
-    int menu;
-    printf("\n*** 점수계산기 ***\n");
-    printf("1. 조회\n");
-    printf("2. 추가\n");
-    printf("3. 수정\n");
-    printf("4. 삭제\n");
-    printf("5. 별점이 4점 이상인 제품\n");
-    printf("6. 별점이 2점 이하인 제품\n");
-    printf("7. 저장\n");
-    printf("0. 종료\n\n");
-    printf("=> 원하는 메뉴는? ");
-    scanf("%d", &menu);
-    return menu;
+void bestProduct(Product *p, int count){
+    printf("별점이 4점 이상인 제품은\n");
+    for(int i=0; i<count; i++){
+        if(p[i].num >= 4){
+                printf("%s: %d\n", p[i].name, p[i].num);
+         }
+    }
 }
 
-void saveData(Product *p, int count){
- 	FILE *fp;
- 	fp = fopen("product.txt", "wt");
+void worstProduct(Product *p, int count){
+    printf("별점이 2점 이하인 제품은\n");
+    for(int i=0; i<count; i++){
+        if(p[i].num <= 2){
+                printf("%s: %d\n", p[i].name, p[i].num);
+        }
+    }
+}
 
- 	for(int i =0; i<count; i++){
-		if(p[i].weight == -1) continue;
-		fprintf(fp, "%s %d %d %d\n", p[i].name, p[i].weight, p[i].price, p[i].num);
-	}
-	fclose(fp);
-	printf("=>저장됨!\n");
+
+void saveData(Product *p, int count){
+        FILE *fp;
+        fp = fopen("product.txt", "wt");
+
+        for(int i =0; i<count; i++){
+                if(p[i].weight == -1) continue;
+                fprintf(fp, "%s %d %d %d\n", p[i].name, p[i].weight, p[i].price, p[i].num);
+        }
+        fclose(fp);
+        printf("=>저장됨!\n");
 }
 
 int loadData(Product *p){
- 	int count =0, i=0;
- 	FILE *fp;
- 	fp = fopen("product.txt", "rt");
- 	if(fp == NULL){
-		printf("=> 파일 없음\n");
-  	}else{
-		for(; i<100; i++){
-			fscanf(fp, "%s", p[i].name);
-			if(feof(fp)) break;
-			fscanf(fp, "%d", &p[i].weight);
-			fscanf(fp, "%d", &p[i].price);
-			fscanf(fp, "%d", &p[i].num);
-		}
-		printf("=> 로딩성공!\n");
-	}
-	fclose(fp);
+        int count =0, i=0;
+        FILE *fp;
+        fp = fopen("product.txt", "rt");
+        if(fp == NULL){
+                printf("=> 파일 없음\n");
+        }else{
+                for(; i<100; i++){
+                        fscanf(fp, "%s", p[i].name);
+                        if(feof(fp)) break;
+                        fscanf(fp, "%d", &p[i].weight);
+                        fscanf(fp, "%d", &p[i].price);
+                        fscanf(fp, "%d", &p[i].num);
+                }
+                printf("=> 로딩성공!\n");
+        }
+        fclose(fp);
 
-	return i;
+        return i;
+}
+
+void searchData(Product *p, int count){
+    int sweight;
+    int check =0;
+    printf("찾는 제품의 중량은?\n");
+    scanf("%d", &sweight);
+    for(int i=0; i<count; i++){
+        if(p[i].weight == sweight){
+            printf("%s: %d\n", p[i].name, p[i].num);
+            check =1;
+        }
+    }
+    if (check ==0){
+        printf("찾는 내용이 없습니다.");
+    }
 }
