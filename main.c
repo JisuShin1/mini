b5fb6b99f36c9fecb58f6cb0b20d86475fb3f76b
Jisu <22000392@handong.edu>
2021-04-18 19:05:38 +0900

    savedata in process

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include "product.h"
+#include "manager.h"
+
+int main(){ 
+    Product slist[20];
+    int index=0;
+    int count=0, menu;
+    index = count;
+
+    while(1){
+        menu=selectMenu();
+        if(menu==0) {
+          printf("종료됨!");
+          break;
+        }
+        if(menu==1 || menu==3 || menu==4)
+            if(count==0){   
+                printf("데이터가 없습니다.\n");
+                continue;
+            }
+        if(menu==2){
+            createProduct(&slist[index++]); 
+            count=1;
+        }
+        else if(menu==1) {
+          if(count>0) listProduct(slist, index);
+        }
+        else if(menu==3) {
+            int no = selectDataNo(slist, index);
+            if (no>0)
+            UpdateProduct(&slist[no-1]);
+        }
+        else if(menu==4) {
+          int no = selectDataNo(slist, index);
+          if (no>0)
+          deleteProduct(&slist[no-1]);
+          count=0;
+        }
+	else if(menu==5){
+		bestProduct(slist, index);
+	}
+	else if(menu==6){
+		worstProduct(slist, index);
+	}
+
+
+    }
+    return 0;
+}
